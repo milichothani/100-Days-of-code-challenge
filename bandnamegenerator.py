bba6483f65f5761c98f2b99ddbc023492d9f1a3c
milichothani <milichothani4@gmail.com>
2024-12-21 19:47:15 +0530

    Initial commit

---
+++ bandnamegenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+name = input("\nEnter the name of the city you grew up in\n")
+pet_name = input("\nEnter the name of your pet\n")
+print("Your band name would be" + " " + name + " " + pet_name )
